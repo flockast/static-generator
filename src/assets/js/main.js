3272015ff73b08dc8543162e8710349d84848d70
konstantin <flockast@gmail.com>
2018-10-27 13:01:58 +0300

    add eslinter

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,2 @@
 const sum = (...numbers) => numbers.reduce((a, b) => a + b);
-console.log("yo1", sum(1, 2, 3, 4, 5, 6, 7, 8));
-console.log("yoooo man", sum(1, 2, 3, 4, 5, 6, 7, 8));
+sum(1, 2, 3, 4, 5, 6);
